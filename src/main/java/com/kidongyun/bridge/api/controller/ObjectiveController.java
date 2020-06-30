0601446e67cf300df18c5e1710d82ed23503adb1
Kidong Yun <oriondigestive@gmail.com>
2020-06-30 12:07:39 +0900

    0630

--- src/main/java/com/kidongyun/bridge/api/controller/ObjectiveController.java
+++ src/main/java/com/kidongyun/bridge/api/controller/ObjectiveController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -57,6 +59,21 @@ public class ObjectiveController {
 
         System.out.println("KIDONG : " + obj.toString());
         objectiveList.add(obj);
+
+        return new Gson().toJson(objectiveList);
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @DeleteMapping(value = "/deleteObj", produces = "application/json;charset=UTF-8")
+    public String deleteObj(@RequestBody Map<String, Integer> param) {
+        int id = param.get("id");
+
+        List<Objective> filtered = objectiveList.stream()
+                .filter(obj -> obj.getId() != id)
+                .collect(Collectors.toList());
+
+        objectiveList = filtered;
 
         return new Gson().toJson(objectiveList);
     }
